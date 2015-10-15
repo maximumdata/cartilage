3ef25048dce0d5c4b9b780d108554d72bd5f62a6
Mike Dettmer <mike@mikedettmer.com>
2015-10-15 19:16:52 +0000

    Updated functions.php to enqueue the google cdn version of jquery

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ function google_jquery_enqueue() {
 
 function theme_enqueues() {
 	wp_enqueue_style( 'compiled-styles', get_template_directory_uri() . '/public/css/style.css' );
-	wp_enqueue_script( 'vendor', get_template_directory_uri() . '/public/js/vendor.js', array(), '1.0.0', true );
+	wp_enqueue_script( 'vendor', get_template_directory_uri() . '/public/js/vendor.js', array('jquery'), '1.0.0', true );
 	wp_enqueue_script( 'custom', get_template_directory_uri() . '/public/js/custom.js', array('vendor'), '1.0.0', true );
 }
 
