128ba6b0c96f8c8f75b4c739f5c19fae34c9a851
Mike Dettmer <mike@mikedettmer.com>
2015-11-06 15:07:53 +0000

    update to header.php

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title><?php wp_title(''); ?></title>
+  <title><?php bloginfo('name'); ?> | <?php is_front_page() ? bloginfo('description') : wp_title(''); ?></title>
   <?php wp_head(); ?>
 </head>
 <body>
